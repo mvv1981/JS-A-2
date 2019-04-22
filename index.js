378da539824b7674e208593853bcd9a8afe7c7a7
Lebed-kun <lebed-kun@mail.ru>
2019-04-22 21:38:16 +0400

    Task done

--- index.js
+++ index.js
@@ -3,5 +3,23 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
+  let results = [];
+  let count = operations.length;
+  let isError = false;
 
+  for (let i = 0; i < operations.length; i++) {
+    operations[i]((err, res) => {
+      if (isError) return;
+
+      if (err) {
+        isError = true;
+        callback(err);
+      } else if (count == 0) {
+        callback(null, results);
+      } else {
+        results.push(res);
+        count--;
+      }
+    });
+  }
 };
