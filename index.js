d34914e4ab577f0767f8b7c1ec78136f511a7cd5
max-barabash <48650035+max-barabash@users.noreply.github.com>
2019-04-27 22:25:17 +0400

    Update function

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 /**
+ * Барабаш Максим Сергеевич
  * @param {Function[]} operations
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
-
+    let promiseList = [];
+    for (let i = 0; i < operations.length; i++) {
+        let promise = new Promise ((resolve, reject) => {
+            operations[i]((err, res) => {
+                if(err === null) {
+                    resolve(res)
+                }
+                else
+                    reject(err)
+            });
+        });
+        promiseList.push(promise);
+    }
+    Promise
+        .all(promiseList)
+        .then((res) => {callback(null, res)}, () => {callback('ERROR')});
 };
